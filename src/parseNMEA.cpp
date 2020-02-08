12b37b98023cfd6f7fef94a609a1503ee9936426
PetarPeychev <petarpeychev98@gmail.com>
2020-02-08 20:40:53 +0000

    implemented the hasValidChecksum function and removed capturing groups from regex in isWellFormedSentence

--- src/parseNMEA.cpp
+++ src/parseNMEA.cpp
@@ -2,20 +2,30 @@
 #include "parseNMEA.h"
 
 #include <regex>
+#include <cassert>
+#include <numeric>
+#include <functional>
 
 namespace NMEA
 {
 
   bool isWellFormedSentence(std::string sentence)
   {
-      std::regex sentence_pattern("^\\$GP([A-Z]{3})((?:,[^,\\*$]*)*)\\*([0-9a-fA-F]{2})");
-      return std::regex_match(sentence, sentence_pattern);
+      std::regex well_formed_pattern("^\\$GP[A-Z]{3}(?:,[^,\\*$]*)*\\*[0-9a-fA-F]{2}");
+      return std::regex_match(sentence, well_formed_pattern);
   }
 
-  bool hasValidChecksum(std::string)
+  bool hasValidChecksum(std::string sentence)
   {
-      // Stub definition, needs implementing
-      return false;
+      assert(isWellFormedSentence(sentence));
+
+      std::regex checksum_pattern("^\\$(GP[A-Z]{3}(?:,[^,\\*$]*)*)\\*([0-9a-fA-F]{2})");
+      std::smatch matches;
+      std::regex_match (sentence, matches, checksum_pattern);
+      std::string chars = matches[1];
+      int checksum = stoi(matches[2], nullptr, 16);
+      int parity = std::accumulate(chars.begin(), chars.end(), 0, std::bit_xor<int>());
+      return parity == checksum;
   }
 
   SentenceData extractSentenceData(std::string)
